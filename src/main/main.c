7cd2c484b7f83be3946b65c83e737012600ec970
ljhm <9011764+ljhm@users.noreply.github.com>
2023-03-03 16:02:02 +0800

    Add files via upload

--- src/main/main.c
+++ src/main/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <signal.h>
+#include <sanitizer/lsan_interface.h>
 #include "foo.h"
 
+void handlerCont(int signum) {
+  printf("SIGCONT %d\n", signum);
+#ifndef NDEBUG
+  __lsan_do_recoverable_leak_check();
+#endif
+}
+
 int main() {
-    printf("main\n");
-    foo();
+  signal(SIGCONT, handlerCont); // kill -CONT 123 # pid
+  //...
+  printf("main\n");
+  foo();
+  int a[1024];
+  int n = 102400;
+  a[n] = 1;
+
 }
